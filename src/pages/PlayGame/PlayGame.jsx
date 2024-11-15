fbfd5019daa1ecae9dd0676cd3aedd33af388607
TahsinZahir <tahsindbg0142@gmail.com>
2024-11-15 18:15:22 +0530

    Added letter buttons

--- src/pages/PlayGame/PlayGame.jsx
+++ src/pages/PlayGame/PlayGame.jsx
@@ -1,13 +1,21 @@
 import { Link } from "react-router-dom";
 import MaskedText from "../../components/MaskedText/MaskedText";
-function PlayGame(){
-return(
-<div>
-    <h1>Play Game</h1>
-    <MaskedText text="humble" usedLetters={['b','e']}/>
-    <hr/>
-    <Link to='/start' className="text-blue-600 underline hover:text-blue-700"> Start Game </Link>
-</div>
-);
+import LetterButtons from "../../components/LetterButtons/LetterButtons";
+
+function PlayGame() {
+
+    return (
+        <div>
+            <h1>Play Game</h1>
+            
+            <MaskedText text="humble" usedLetters={['b', 'e']} />
+            <hr/>
+
+            <LetterButtons usedLetters={['b', 'e']} />
+            <hr />
+
+            <Link to='/start'> Start Game </Link>
+        </div>
+    );
 }
 export default PlayGame;
